282c621d0629172dc4a5a3e1ff93b8432a5c3c20
mixinspace <islammadkaur@gmail.com>
2020-11-19 16:59:26 -0600

    commit tree 6484555f91b1fe8a003d93c5df47dbd88521e0b9

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      
+      erb :'pirates/new'
+    end
+    
   end
+
 end
